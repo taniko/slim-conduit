f8b330302e6460bfba29c1c5434f13bce859cc42
Yuya Aratani <hrgruri@gmail.com>
2020-10-17 21:02:34 +0900

    add feature of delete article

--- src/Domain/Model/Article/ArticleRepository.php
+++ src/Domain/Model/Article/ArticleRepository.php
@@ -4,8 +4,18 @@
 namespace App\Domain\Model\Article;
 
 
+use App\Domain\DomainException\DomainRecordNotFoundException;
+
 interface ArticleRepository
 {
     public function save(Article $article);
-    public function findById(int $id): ?Article;
+
+    /**
+     * @param int $id
+     * @return Article
+     * @throws DomainRecordNotFoundException
+     */
+    public function findById(int $id): Article;
+
+    public function delete(int $id);
 }
